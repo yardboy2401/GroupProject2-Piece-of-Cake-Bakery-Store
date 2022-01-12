fc4d1295da88ef829736d41bea5c5397b95f347f
Jeff Wymore <yardboy2401@gmail.com>
2022-01-11 21:49:35 -0800

    store filename change for pics in seed file

--- seeds/storeSeeds.js
+++ seeds/storeSeeds.js
@@ -7,55 +7,55 @@ const storeData = [
       "title": "Chocolate Almond Croissant",
       "description": "A wonderful pastry to go with your morning coffee or anytime really!",
       "price": "3.49",
-      "imgName": "chocolatealmondcroissant.jpg"
+      "imgName": "chocolatealmondcroissant.JPG"
     },
     {
         "title": "Maple Bar",
         "description": "A classic morning treat!",
         "price": "1.49",
-        "imgName": "maplebar.JPG"
+        "imgName": "maplebar.jpg"
     },
     {
         "title": "Baguette",
         "description": "A delicious small crusty loaf of heaven!",
         "price": "2.39",
-        "imgName": "baguette.JPG"
+        "imgName": "baguette.jpg"
     },
     {
         "title": "Chocolate Brownie",
         "description": "An amazing little square of the best chocolate brownie!",
         "price": "1.99",
-        "imgName": "brownies.JPG"
+        "imgName": "brownies.jpg"
     },
     {
         "title": "Cake Donut",
         "description": "A wonderful little creation to go with your morning coffee or anytime really!",
         "price": "1.29",
-        "imgName": "cakedonuts.JPG"
+        "imgName": "cakedonuts.jpg"
     },
     {
         "title": "Croissant",
         "description": "A wonderful pastry to go with your morning coffee or anytime really!",
         "price": "2.99",
-        "imgName": "croissant.JPG"
+        "imgName": "croissant.jpg"
     },
     {
         "title": "Lemon Blueberry Scone",
         "description": "A delightful bite of Lemon Blueberry Bliss!",
         "price": "2.99",
-        "imgName": "scones.JPG"
+        "imgName": "scones.jpg"
     },
     {
         "title": "Sourdough Bread Loaf",
         "description": "A an amazing loaf of Sourdough Bread!",
         "price": "3.99",
-        "imgName": "sourdoughloaf.JPG"
+        "imgName": "sourdoughloaf.jpg"
     },
     {
         "title": "Chocolate Cupcake",
         "description": "A delicious treat anytime!",
         "price": "1.49",
-        "imgName": "cupcake.JPG"
+        "imgName": "cupcake.jpg"
     },
   ]
 
